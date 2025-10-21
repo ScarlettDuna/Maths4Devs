27c95719c4710f1b5125012c9097c321c03cf401
ScarlettDuna <arantxa.rg.1991@hotmail.com>
2025-10-21 12:05:51 +0200

    unsaved changes

--- Bloque2/Nivel5-Desafios-Matematicos.py
+++ Bloque2/Nivel5-Desafios-Matematicos.py
@@ -1,8 +1,38 @@
+from functools import reduce 
 # Implementa reduce para calcular una serie de Fibonacci de longitud n.
+# 1 1 2 3 5 8 13 21 
+def fibonacci(n):
+    fibo_array = [1]
+    for i in range(1, n):
+        if i == 1:
+            fibo_array.append(i)
+        else:
+            fibo_array.append(fibo_array[i-2] + fibo_array[i-1])
+    return fibo_array
+print(fibonacci(9))
 
+def fibo(n):
+    return reduce(
+        lambda acc, _: (acc.append(acc[-2] + acc[-1]) or acc), range(n - 2), [1, 1])
+
+fibonacci_array = fibo(9)
+print(fibonacci_array)
 # Usa reduce para generar el número binario correspondiente a una lista de bits ([1,0,1,1] → 11).
+def bin2dec(array):
+    dec = 0
+    for i in range(len(array)):
+        dec += pow(array[i] * 2, len(array) - 1 - i) 
+    return dec
+print(bin2dec([1,0,1,1]))
+
+decimal = reduce(lambda acc, num: acc * 2 + num, [1,0,1,1], 0)
+print(decimal)
 
 # Calcula el producto escalar entre dos listas de números.
+a = [5, 9, 7, 10]
+b = [6, 2, 4, 8]
+c = reduce(lambda acc, par: acc + par[0] * par[1], zip(a, b), 0)
+print(c)
 
 # Implementa una función que determine si una cadena es un palíndromo usando reduce.
 
